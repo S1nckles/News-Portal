cfdfaed2b70c4efd84d99f4c5a0dc102c6145ca6
Andrey <ilynych0810@gmail.com>
2025-06-11 18:01:34 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer id="footer">
-    <div class="container">
-        <span class="footer__copyright">&copy; 2025 News Portal</span>
-    </div>
+        <div class="container">
+            <span class="footer__copyright">&copy; 2025 News Portal</span>
+        </div>
     </footer>
     <?php wp_footer(); ?>
 </body>
