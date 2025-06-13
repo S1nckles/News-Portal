260448e7cdd84dfa13d446050197947732d8be58
Andrey <ilynych0810@gmail.com>
2025-06-13 17:51:51 +0200

    Update and refinement of the functionality of search

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,18 @@
-const searchIcon = document.querySelector('.search-btn');
-const searchInput = document.querySelector('.search-input');
+document.addEventListener("DOMContentLoaded", function () {
+  const searchForm = document.querySelector('.nav__search');
+  const input = searchForm.querySelector('.search-input');
+  const btn = searchForm.querySelector('.search-btn');
 
-searchIcon.addEventListener('click', () => {
-  searchInput.classList.toggle('active');
+  let expanded = false;
+
+  btn.addEventListener('click', function (e) {
+    if (!expanded) {
+      e.preventDefault();
+      input.classList.add('active');
+      input.focus();
+      expanded = true;
+    } else if (!input.value.trim()) {
+      e.preventDefault();
+    }
+  });
 });
